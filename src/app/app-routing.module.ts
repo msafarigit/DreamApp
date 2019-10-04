932197597dcd818dea080859cbf0f19680e239fb
msafarigit <msafari.int@gmail.com>
2019-10-04 11:56:57 +0330

    lifecycle first step

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ModalComponent } from './modal/modal.component';
 import { StructuralDirectiveComponent } from './structural-directive/structural-directive.component';
 import { AttributeDirectiveComponent } from './attribute-directive/attribute-directive.component';
 import { TemplateReferenceVariableComponent } from './template-reference-variable/template-reference-variable.component';
+import { LifecycleComponent } from './lifecycle/lifecycle.component';
 
 
 const router: Routes = [
   { path: 'modal', component: ModalComponent },
   { path: 'structural', component: StructuralDirectiveComponent },
   { path: 'attribute', component: AttributeDirectiveComponent },
-  { path: 'templateReferenceVariable', component: TemplateReferenceVariableComponent }
+  { path: 'templateReferenceVariable', component: TemplateReferenceVariableComponent },
+  { path: 'lifecycle', component: LifecycleComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ModalComponent } from './modal/modal.component';
 import { StructuralDirectiveComponent } from './structural-directive/structural-directive.component';
 import { AttributeDirectiveComponent } from './attribute-directive/attribute-directive.component';
+import { TemplateReferenceVariableComponent } from './template-reference-variable/template-reference-variable.component';
+import { LifecycleComponent } from './lifecycle/lifecycle.component';
 
 import { HighlightDirective } from './directive/highlight.directive';
-import { TemplateReferenceVariableComponent } from './template-reference-variable/template-reference-variable.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { TemplateReferenceVariableComponent } from './template-reference-variabl
     StructuralDirectiveComponent,
     AttributeDirectiveComponent,
     TemplateReferenceVariableComponent,
+    LifecycleComponent,
 
 
 
