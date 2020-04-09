416003d761c67cfec32eb248e7ff4f80c65050f5
msafarigit <msafari.int@gmail.com>
2020-04-09 18:09:37 +0430

    continue implement model driven validators

--- src/app/component/form/model-driven-validator/model-driven-validator.component.ts
+++ src/app/component/form/model-driven-validator/model-driven-validator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 
 import { ageRangeValidator, ageCustomRangeValidator } from '@shared/validator/age-range-validator';
+import { AppValidators } from '@shared/validator/app-validators';
 
 @Component({
   selector: 'app-model-driven-validator',
@@ -17,7 +18,8 @@ export class ModelDrivenValidatorComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.validationForm = this.fb.group({
       age: new FormControl('', [Validators.required, ageRangeValidator]),
-      customAge: new FormControl('20', ageCustomRangeValidator(20, 30))
+      customAge: new FormControl('20', ageCustomRangeValidator(20, 30)),
+      email: new FormControl('', [Validators.required, Validators.email, AppValidators.emailDomainValidator('gmail.com')])
     });
   }
 
---
+++ src/app/shared/validator/app-validators.ts
@@ -0,0 +1,19 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms'
+
+export class AppValidators {
+
+  static emailDomainValidator(customDomain: string): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      if (!control.value)
+        return null;
+
+      const email = control.value;
+      if (email.indexof('@') === -1)
+        return { emailDomain: false };
+
+      const [_, domain] = email.split('@'); // array destructor
+      return domain === customDomain ? null : { emailDomain: false };
+    }
+  }
+
+}
