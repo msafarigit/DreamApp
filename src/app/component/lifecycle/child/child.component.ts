54b8c6d32203ea3bde1f43b172d56009fdfa0f24
msafarigit <msafari.int@gmail.com>
2020-05-29 10:08:20 +0430

    recover session 3 ended

--- src/app/component/lifecycle/child/child.component.ts
+++ src/app/component/lifecycle/child/child.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input, DoCheck } from '@angular/core';
+import { Person } from '@model/Person';
 
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges, OnInit, DoCheck {
 
-  constructor() { }
+  @Input() person: Person;
+  @Input() master: string;
 
-  ngOnInit(): void {
+  changeLog: string[];
+  private oldPersonName: string;
+  private oldMaster: string;
+  private changeDetected = false;
+  private noChangeCount = 0;
+
+  constructor() {
+    this.changeLog = [];
+    this.changeLog.push(`constructor: person value ${this.person}`); // person has undefined
   }
 
+  // ngOnChanges(changes: import('@angular/core').SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    // this.changeLog.push(`ngOnChanges: person value ${JSON.stringify(this.person)}`); // person has value
+
+    for (const inputName in changes) {
+      if (changes.hasOwnProperty(inputName)) {
+        const change = changes[inputName];
+        const prevValue = JSON.stringify(change.previousValue);
+        const currentValue = JSON.stringify(change.currentValue);
+
+        if (change.isFirstChange()) {
+          this.changeLog.push(`ngOnChanges isFirstChange: ${inputName}, Initial value ${prevValue} -----> ${currentValue}`);
+        } else {
+          this.changeLog.push(`ngOnChanges: ${inputName}, Initial value ${prevValue} -----> ${currentValue}`);
+        }
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.changeLog.push(`ngOnInit: person value ${JSON.stringify(this.person)}`);
+  }
+
+  ngDoCheck(): void {
+    if (this.person.name !== this.oldPersonName) {
+      this.changeDetected = true;
+      this.changeLog.push(`ngDoCheck: PersonName ${this.oldPersonName} ---> ${this.person.name}`);
+      this.oldPersonName = this.person.name;
+    }
+
+    if (this.master !== this.oldMaster) {
+      this.changeDetected = true;
+      this.changeLog.push(`ngDoCheck: Master ${this.oldMaster} ---> ${this.master}`);
+      this.oldMaster = this.master;
+    }
+
+    if (this.changeDetected) {
+      this.noChangeCount = 0;
+    } else {
+      const count = this.noChangeCount += 1;
+      const noChangeMsg = `ngDoCheck: called ${count} with no change`;
+
+      if (count === 1) {
+        this.changeLog.push(noChangeMsg);
+      } else {
+        this.changeLog[this.changeLog.length - 1] = noChangeMsg;
+      }
+    }
+
+    this.changeDetected = false;
+  }
 }
--- src/app/component/lifecycle/home/home.component.ts
+++ src/app/component/lifecycle/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { PERSONS, Person } from '@model/Person';
 
 // tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
@@ -9,11 +10,14 @@ import { Component, OnInit, OnChanges, SimpleChanges, DoCheck, AfterContentInit,
 export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   callStack: Array<string>;
+  selectedPerson: Person;
+  master: string;
 
   constructor() {
     this.callStack = new Array<string>();
     this.callStack.push('constructor');
 
+    this.selectedPerson = PERSONS[0];
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -73,4 +77,23 @@ export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentIn
 
 // ngOnDestroy(): Cleanup just before Angular destroys the directive/component. Unsubscribe Observables and detach event handlers to avoid memory leaks.
 //                Called just before Angular destroys the directive/component.
+*/
+
+/*
+Initializing a component or directive:
+Use the ngOnInit() method to perform the following initialization tasks.
+
+1- Perform complex initializations outside of the constructor. Components should be cheap and safe to construct.
+ You should not, for example, fetch data in a component constructor.
+ You shouldn't worry that a new component will try to contact a remote server when created under test or before you decide to display it.
+ An ngOnInit() is a good place for a component to fetch its initial data. For an example, see the Tour of Heroes tutorial.
+
+ url:http://misko.hevery.com/code-reviewers-guide/flaw-constructor-does-real-work/
+ In Flaw: Constructor does Real Work, Misko Hevery, Angular team lead, explains why you should avoid complex constructor logic.
+
+2- Set up the component after Angular sets the input properties. Constructors should do no more than set the initial local variables to simple values.
+ Keep in mind that a directive's data-bound input properties are not set until after construction. If you need to initialize the directive based on those properties,
+ set them when ngOnInit() runs.
+The ngOnChanges() method is your first opportunity to access those properties.
+Angular calls ngOnChanges() before ngOnInit(), but also many times after that. It only calls ngOnInit() once.
 */
