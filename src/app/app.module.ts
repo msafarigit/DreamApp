3a04ac674c0c392aa898a3049a2e0d02403c6f04
msafarigit <msafari.int@gmail.com>
2020-11-18 15:57:36 +0330

    add usefull comments about Angular Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -88,48 +88,112 @@ Any other import is considered non-relative. Some examples include:
 */
 
 /*
+We have 3 types of modules by The Angular style guide:
+ -Feature modules
+ -Core Module
+ -Shared Module
+
+ 1-Feature Modules: those are the modules that encapsulate a specific feature at a logic level, for example you have a dashboard page
+  that allows the users to see their projects.
+
+ 2-Core Module: Here we include functionality that will be used only ONCE, The Core module is used ONLY in the root (app) Module!
+  Common services are placed in the Core Module to ensure we have only a single instance of the services to avoid unexpected behaviors.
+
+  import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
+  export class CoreModule extends EnsureModuleLoadedOnceGuard {
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+       super(parentModule);
+    }
+  }
+
+ 3-Shared Module: We use the SharedModule in the feature modules to make common functionality available. We also make sure to have only one Shared Module.
+  We avoid placing services here! This is the most missunderstooded kind of Module! The purpose of the SharedModule is to make available commonly used:
+   - components
+   - directives
+   - pipes
+
+ Note the following:
+  - It imports the CommonModule because the module's component needs common directives.
+  - It declares and exports the utility pipe, directive, and component classes.
+  - It re-exports the CommonModule and FormsModule.
+
+ @NgModule({
+  imports:      [ CommonModule ],
+  declarations: [ CustomerComponent, NewItemDirective, OrdersPipe ],
+  exports:      [ CustomerComponent, NewItemDirective, OrdersPipe, CommonModule, FormsModule ]
+ })
+ export class SharedModule { }
+
+ Using components vs services from other modules:
+  There is an important distinction between using another module's component and using a service from another module.
+  Import modules when you want to use directives, pipes, and components.
+  Importing a module with services means that you will have a new instance of that service,
+  which typically is not what you need (typically one wants to reuse an existing service). Use module imports to control service instantiation.
+*/
+
+/*
+ An NgModule describes how the application parts fit together. Every application has at least one Angular module,
+ the root module, which must be present for bootstrapping the application on launch. By convention and by default, this NgModule is named AppModule.
+ The @NgModule decorator identifies AppModule as an NgModule class. @NgModule takes a metadata object that tells Angular how to compile and launch the application.
+  -declarations: this application's lone component. declarations: [
+     YourComponent,
+     YourPipe,
+     YourDirective
+    ]
+  -imports: import BrowserModule to have browser specific services such as DOM rendering, sanitization, and location. The module's imports array
+   appears exclusively in the @NgModule metadata object. It tells Angular about other NgModules that this particular module needs to function properly.
+  -providers: the service providers.
+  -bootstrap: the root component that Angular creates and inserts into the index.html host web page.
+
+-Feature modules vs. root modules:
+  A feature module is an organizational best practice, as opposed to a concept of the core Angular API.
+  A feature module delivers a cohesive set of functionality focused on a specific application need such as a user workflow, routing, or forms.
+   While you can do everything within the root module, feature modules help you partition the app into focused areas.
+  A feature module collaborates with the root module and with other modules through the services it provides and the components, directives,
+  and pipes that it shares.
+
 There are five general categories of feature modules which tend to fall into the following groups:
-  Domain feature modules.
-  Routed feature modules.
-  Routing modules.
-  Service feature modules.
-  Widget feature modules.
+ -Domain feature modules.
+ -Routed feature modules.
+ -Routing modules.
+ -Service feature modules.
+ -Widget feature modules.
 
 1- Domain feature modules deliver a user experience dedicated to a particular application domain like editing a customer or placing an order.
-They typically have a top component that acts as the feature root and private, supporting sub-components descend from it.
-Domain feature modules consist mostly of declarations. Only the top component is exported.
-Domain feature modules rarely have providers. When they do, the lifetime of the provided services should be the same as the lifetime of the module.
-Domain feature modules are typically imported exactly once by a larger feature module.
-They might be imported by the root AppModule of a small application that lacks routing.
+ They typically have a top component that acts as the feature root and private, supporting sub-components descend from it.
+ Domain feature modules consist mostly of declarations. Only the top component is exported.
+ Domain feature modules rarely have providers. When they do, the lifetime of the provided services should be the same as the lifetime of the module.
+ Domain feature modules are typically imported exactly once by a larger feature module.
+ They might be imported by the root AppModule of a small application that lacks routing.
 
 2- Routed feature modules are domain feature modules whose top components are the targets of router navigation routes.
-All lazy-loaded modules are routed feature modules by definition.
-Routed feature modules don’t export anything because their components never appear in the template of an external component.
-A lazy-loaded routed feature module should not be imported by any module. Doing so would trigger an eager load,
- defeating the purpose of lazy loading.That means you won’t see them mentioned among the AppModule imports.
+ All lazy-loaded modules are routed feature modules by definition.
+ Routed feature modules don’t export anything because their components never appear in the template of an external component.
+ A lazy-loaded routed feature module should not be imported by any module. Doing so would trigger an eager load,
+  defeating the purpose of lazy loading.That means you won’t see them mentioned among the AppModule imports.
  An eager loaded routed feature module must be imported by another module so that the compiler learns about its components.
-Routed feature modules rarely have providers for reasons explained in Lazy Loading Feature Modules.
+ Routed feature modules rarely have providers for reasons explained in Lazy Loading Feature Modules.
  When they do, the lifetime of the provided services should be the same as the lifetime of the module.
  Don't provide application-wide singleton services in a routed feature module or in a module that the routed module imports.
 
 3- A routing module provides routing configuration for another module and separates routing concerns from its companion module.
-A routing module typically does the following:
- - Defines routes.
+ A routing module typically does the following:
+  - Defines routes.
 
- - Adds router configuration to the module's imports.
+  - Adds router configuration to the module's imports.
 
- - Adds guard and resolver service providers to the module's providers.
+  - Adds guard and resolver service providers to the module's providers.
 
- - The name of the routing module should parallel the name of its companion module, using the suffix "Routing".
-  For example, FooModule in foo.module.ts has a routing module named FooRoutingModule in foo-routing.module.ts.
-  If the companion module is the root AppModule, the AppRoutingModule adds router configuration to its imports with RouterModule.forRoot(routes).
-  All other routing modules are children that import RouterModule.forChild(routes).
+  - The name of the routing module should parallel the name of its companion module, using the suffix "Routing".
+   For example, FooModule in foo.module.ts has a routing module named FooRoutingModule in foo-routing.module.ts.
+   If the companion module is the root AppModule, the AppRoutingModule adds router configuration to its imports with RouterModule.forRoot(routes).
+   All other routing modules are children that import RouterModule.forChild(routes).
 
- - A routing module re-exports the RouterModule as a convenience so that components of the companion module have access to router directives such as RouterLink and RouterOutlet.
+  - A routing module re-exports the RouterModule as a convenience so that components of the companion module have access to router directives such as RouterLink and RouterOutlet.
 
- - A routing module does not have its own declarations. Components, directives, and pipes are the responsibility of the feature module, not the routing module.
+  - A routing module does not have its own declarations. Components, directives, and pipes are the responsibility of the feature module, not the routing module.
 
-A routing module should only be imported by its companion module.
+ A routing module should only be imported by its companion module.
 
 4- Service modules provide utility services such as data access and messaging.
  Ideally, they consist entirely of providers and have no declarations. Angular's HttpClientModule is a good example of a service module.
