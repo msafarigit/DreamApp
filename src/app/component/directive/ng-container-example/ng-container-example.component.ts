ebd25f3a30d8cd5be2202f2ac13f94fae8defc84
msafarigit <msafari.int@gmail.com>
2020-11-18 17:10:56 +0330

    add usefully comments about ng-container

--- src/app/component/directive/ng-container-example/ng-container-example.component.ts
+++ src/app/component/directive/ng-container-example/ng-container-example.component.ts
@@ -131,5 +131,43 @@ export class NgContainerExampleComponent {
   checkIndex(index: number) {
     return Math.abs(this.currentPage - index) < 5;
   }
-
 }
+
+/*
+<ng-container> to the rescue
+ The Angular <ng-container> is a grouping element that doesn't interfere with styles or layout because Angular doesn't put it in the DOM.
+ The <ng-container> is a syntax element recognized by the Angular parser. It's not a directive, component, class, or interface.
+
+ ngif-ngcontainer
+ <p>
+  I turned the corner
+  <ng-container *ngIf="hero">
+    and saw {{hero.name}}. I waved
+  </ng-container>
+  and continued on my way.
+</p>
+
+ It's more like the curly braces in a JavaScript if-block:
+ if (someCondition) {
+  statement1;
+  statement2;
+  statement3;
+}
+
+Without those braces, JavaScript would only execute the first statement when you intend to conditionally execute all of them as a single block.
+The <ng-container> satisfies a similar need in Angular templates.
+
+select-ngcontainer:
+
+<div>
+  Pick your favorite hero
+  (<label><input type="checkbox" checked (change)="showSad = !showSad">show sad</label>)
+</div>
+<select [(ngModel)]="hero">
+  <ng-container *ngFor="let h of heroes">
+    <ng-container *ngIf="showSad || h.emotion !== 'sad'">
+      <option [ngValue]="h">{{h.name}} ({{h.emotion}})</option>
+    </ng-container>
+  </ng-container>
+</select>
+*/
