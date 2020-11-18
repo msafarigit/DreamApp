1d49836ab4c8effe3378d7e6e2a753efe1deb28b
msafarigit <msafari.int@gmail.com>
2020-11-18 21:53:40 +0330

    add ngContainer useful comments

--- src/app/component/directive/ng-container-example/ng-container-example.component.ts
+++ src/app/component/directive/ng-container-example/ng-container-example.component.ts
@@ -134,40 +134,57 @@ export class NgContainerExampleComponent {
 }
 
 /*
-<ng-container> to the rescue
- The Angular <ng-container> is a grouping element that doesn't interfere with styles or layout because Angular doesn't put it in the DOM.
- The <ng-container> is a syntax element recognized by the Angular parser. It's not a directive, component, class, or interface.
+ <ng-container> to the rescue:
+  The Angular <ng-container> is a grouping element that doesn't interfere with styles or layout because Angular doesn't put it in the DOM.
+  The <ng-container> is a syntax element recognized by the Angular parser. It's not a directive, component, class, or interface.
 
- ngif-ngcontainer
- <p>
-  I turned the corner
-  <ng-container *ngIf="hero">
-    and saw {{hero.name}}. I waved
+  ngif-ngcontainer example:
+  <p>
+   I turned the corner
+   <ng-container *ngIf="hero">
+     and saw {{hero.name}}. I waved
+   </ng-container>
+   and continued on my way.
+  </p>
+
+  It's more like the curly braces in a JavaScript if-block:
+  if (someCondition) {
+   statement1;
+   statement2;
+   statement3;
+  }
+
+  Without those braces, JavaScript would only execute the first statement when you intend to conditionally execute all of them as a single block.
+  The <ng-container> satisfies a similar need in Angular templates.
+
+  -other usage: Let's see what happens if for example we try to use ngIf and ngFor in the same element:
+  <div class="lesson" *ngIf="lessons" *ngFor="let lesson of lessons">
+    <div class="lesson-detail">
+        {{lesson | json}}
+    </div>
+  </div>
+
+  Uncaught Error: Template parse errors:
+  Can't have multiple template bindings on one element. Use only one attribute named 'template' or prefixed with *
+
+  <ng-container *ngIf="lessons">
+    <div class="lesson" *ngFor="let lesson of lessons">
+        <div class="lesson-detail">
+            {{lesson | json}}
+        </div>
+    </div>
   </ng-container>
-  and continued on my way.
-</p>
 
- It's more like the curly braces in a JavaScript if-block:
- if (someCondition) {
-  statement1;
-  statement2;
-  statement3;
-}
-
-Without those braces, JavaScript would only execute the first statement when you intend to conditionally execute all of them as a single block.
-The <ng-container> satisfies a similar need in Angular templates.
-
-select-ngcontainer:
-
-<div>
-  Pick your favorite hero
-  (<label><input type="checkbox" checked (change)="showSad = !showSad">show sad</label>)
-</div>
-<select [(ngModel)]="hero">
-  <ng-container *ngFor="let h of heroes">
-    <ng-container *ngIf="showSad || h.emotion !== 'sad'">
-      <option [ngValue]="h">{{h.name}} ({{h.emotion}})</option>
+  select-ngcontainer example:
+  <div>
+    Pick your favorite hero
+    (<label><input type="checkbox" checked (change)="showSad = !showSad">show sad</label>)
+  </div>
+  <select [(ngModel)]="hero">
+    <ng-container *ngFor="let h of heroes">
+      <ng-container *ngIf="showSad || h.emotion !== 'sad'">
+        <option [ngValue]="h">{{h.name}} ({{h.emotion}})</option>
+      </ng-container>
     </ng-container>
-  </ng-container>
-</select>
+  </select>
 */
